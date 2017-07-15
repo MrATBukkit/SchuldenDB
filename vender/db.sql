111bbd6a4048d48d80c4c8407facd9893577b9c4
MrATBukkit <hewalzl999@gmail.com>
2017-07-15 14:04:08 +0200

    Projekt organisation

---
+++ vender/db.sql
@@ -0,0 +1,49 @@
+-- --------------------------------------------------------
+-- Host:                         localhost
+-- Server Version:               10.2.3-MariaDB-log - mariadb.org binary distribution
+-- Server Betriebssystem:        Win32
+-- HeidiSQL Version:             9.4.0.5125
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+
+-- Exportiere Datenbank Struktur für schuldendb
+CREATE DATABASE IF NOT EXISTS `schuldendb` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `schuldendb`;
+
+-- Exportiere Struktur von Tabelle schuldendb.schulden
+CREATE TABLE IF NOT EXISTS `schulden` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `betrag` double NOT NULL,
+  `intervalTime` int(11) NOT NULL,
+  `startDate` date NOT NULL,
+  `endDate` date NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Daten Export vom Benutzer nicht ausgewählt
+-- Exportiere Struktur von Tabelle schuldendb.schulden_schuldner
+CREATE TABLE IF NOT EXISTS `schulden_schuldner` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `schuldenId` int(11) NOT NULL,
+  `schuldnerId` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Daten Export vom Benutzer nicht ausgewählt
+-- Exportiere Struktur von Tabelle schuldendb.schuldner
+CREATE TABLE IF NOT EXISTS `schuldner` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `Name` varchar(30) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Daten Export vom Benutzer nicht ausgewählt
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
